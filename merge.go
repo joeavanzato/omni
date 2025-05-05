e26360697288436d5b730d45a9fea508cee656bf
panscan <joeavanzato@gmail.com>
2025-05-05 13:59:07 -0400

    Improving logic and adding asym CSV Merge

--- merge.go
+++ merge.go
@@ -8,18 +8,20 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
+	"slices"
 	"strings"
 	"sync"
 )
 
 type MergeFuncs string
 
-var validMergeFuncs = []MergeFuncs{MergeFuncCSV, MergeFuncNone, MergeFuncPool}
+var validMergeFuncs = []MergeFuncs{MergeFuncCSV, MergeFuncNone, MergeFuncPool, MergeFuncAsymcCSV}
 
 const (
-	MergeFuncCSV  MergeFuncs = "csv"
-	MergeFuncNone MergeFuncs = "none"
-	MergeFuncPool MergeFuncs = "pool"
+	MergeFuncCSV      MergeFuncs = "csv"
+	MergeFuncNone     MergeFuncs = "none"
+	MergeFuncPool     MergeFuncs = "pool"
+	MergeFuncAsymcCSV MergeFuncs = "asym_csv"
 )
 
 func doMerges(c Config, aggregateExplicit bool) error {
@@ -44,6 +46,11 @@ func doMerges(c Config, aggregateExplicit bool) error {
 		} else if v.Merge == MergeFuncPool {
 			wg.Add(1)
 			go doPoolMerge(strings.Replace(v.FileName, "$time$", "", 1), "aggregated", &wg, v.AddHostname)
+		} else if v.Merge == MergeFuncAsymcCSV {
+			sourceFile := strings.Replace(v.FileName, "$time$", "", 1)
+			destinationFile := fmt.Sprintf("aggregated\\%s.csv", v.ID)
+			wg.Add(1)
+			go asymmetricCSVMerge(sourceFile, destinationFile, &wg, v.AddHostname)
 		}
 	}
 	wg.Wait()
@@ -83,6 +90,7 @@ func doPoolMerge(sourceFile, destinationDir string, wg *sync.WaitGroup, addhostn
 	}
 }
 
+// doCSVMerge combines symmetric CSV files into a single file - it expects that the files being merged have the same columns
 func doCSVMerge(sourceFile, destinationFile string, wg *sync.WaitGroup, addhostname bool) {
 	defer wg.Done()
 	// Find all files with sourceFile name in devices directory
@@ -103,9 +111,15 @@ func doCSVMerge(sourceFile, destinationFile string, wg *sync.WaitGroup, addhostn
 	}
 	defer fw.Close()
 	defer w.Flush()
+	deviceRegex := regexp.MustCompile(`devices\\([^\\]+)`)
+	headers := make([]string, 0)
 	for _, v := range files {
-		// TODO - Regex to extract ...devices\<hostname>\ instead of assuming we are always at the base
-		currentHost := getLastPathElement(v)
+		currentHost := ""
+		matches := deviceRegex.FindStringSubmatch(v)
+		if len(matches) > 1 {
+			currentHost = matches[1]
+		}
+		//currentHost := getLastPathElement(v)
 		headerDone := false
 
 		func() {
@@ -138,6 +152,9 @@ func doCSVMerge(sourceFile, destinationFile string, wg *sync.WaitGroup, addhostn
 						log.Printf("error writing record: %v (%s)", err, v)
 						break
 					}
+					if !headerDone {
+						headers = record
+					}
 					headerDone = true
 					record_count += 1
 				}
@@ -147,7 +164,7 @@ func doCSVMerge(sourceFile, destinationFile string, wg *sync.WaitGroup, addhostn
 				}
 			} else {
 				// Skip the header
-				_, err := r.Read()
+				headerRow, err := r.Read()
 				if err == io.EOF {
 					return
 				}
@@ -155,6 +172,15 @@ func doCSVMerge(sourceFile, destinationFile string, wg *sync.WaitGroup, addhostn
 					log.Printf("error reading header: %v (%s)", err, v)
 					return
 				}
+				if addhostname {
+					headerRow = append([]string{"PSComputerName"}, headerRow...)
+				}
+
+				if !slices.Equal(headerRow, headers) {
+					log.Printf("error: headers do not match for %s", v)
+					return
+				}
+
 				for {
 					record, err := r.Read()
 					if err == io.EOF {
@@ -173,6 +199,123 @@ func doCSVMerge(sourceFile, destinationFile string, wg *sync.WaitGroup, addhostn
 					}
 				}
 			}
+		}()
+	}
+}
+
+// asymmetricCSVMerge combines CSV files that have varying headers into a single output file - useful for iterating over a complex tool output
+// utilities/CSVMerge.ps1 also achieves this across an entire directory
+func asymmetricCSVMerge(sourceFile, destinationFile string, wg *sync.WaitGroup, addhostname bool) {
+	// We will not read all headers first - instead we will maintain header slice as we parse each file
+	// If header exists in slice, we build record, if not, we add it and build
+	defer wg.Done()
+	// Find all files with sourceFile name in devices directory
+	files, err := findFilesByName("devices", sourceFile, true)
+	if err != nil {
+		log.Printf("error finding files: %v (%s)", err, sourceFile)
+		return
+	}
+	if len(files) == 0 {
+		log.Printf("no files found for %s", sourceFile)
+		return
+	}
+	w, fw, err := createCSVWriter(destinationFile)
+	if err != nil {
+		log.Printf("error creating CSV writer: %v (%s)", err, destinationFile)
+		return
+	}
+	defer fw.Close()
+	defer w.Flush()
+	deviceRegex := regexp.MustCompile(`devices\\([^\\]+)`)
+	// Iterate once to build header structure
+	headers := make([]string, 0)
+	if addhostname {
+		headers = append(headers, "PSComputerName")
+	}
+	for _, v := range files {
+		func() {
+			var r *csv.Reader
+			var fr *os.File
+			r, fr, err = createCSVReader(v)
+			if err != nil {
+				log.Printf("error creating CSV reader: %v (%s)", err, v)
+				return
+			}
+			defer fr.Close()
+
+			headerRow, err := r.Read()
+			if err == io.EOF {
+				return
+			}
+			if err != nil {
+				log.Printf("error reading header: %v (%s)", err, v)
+				return
+			}
+			for _, val := range headerRow {
+				if !slices.Contains(headers, val) {
+					headers = append(headers, val)
+				}
+			}
+		}()
+	}
+	w.Write(headers)
+
+	// Map current headers to their corresponding index in mainHeaders
+	headerMap := make(map[string]int)
+	for i, header := range headers {
+		headerMap[header] = i
+	}
+	// Iterate again to build records
+	for _, v := range files {
+		currentHost := ""
+		if addhostname {
+			matches := deviceRegex.FindStringSubmatch(v)
+			if len(matches) > 1 {
+				currentHost = matches[1]
+			}
+		}
+		func() {
+			headerDone := false
+			headerRow := make([]string, 0)
+			var r *csv.Reader
+			var fr *os.File
+			r, fr, err = createCSVReader(v)
+			if err != nil {
+				log.Printf("error creating CSV reader: %v (%s)", err, v)
+				return
+			}
+			defer fr.Close()
+
+			for {
+				if !headerDone {
+					headerRow, err = r.Read()
+					if err == io.EOF {
+						return
+					}
+					if err != nil {
+						log.Printf("error reading header: %v (%s)", err, v)
+						return
+					}
+					headerDone = true
+				}
+				record, err := r.Read()
+				if err == io.EOF {
+					break
+				}
+				if err != nil {
+					log.Printf("error reading record: %v (%s)", err, v)
+					break
+				}
+				record = buildRecordSlice(headers, headerRow, record, v, headerMap)
+				if addhostname {
+					record = append([]string{currentHost}, record...)
+				}
+				if err := w.Write(record); err != nil {
+					log.Printf("error writing record: %v (%s)", err, v)
+					break
+				}
+			}
+
 		}()
 	}
 
